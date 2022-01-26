1b1a07d64be20896a2509fcdd544620b9c7c463a
kawayiLinLin <1936635053@qq.com>
2022-01-26 18:37:38 +0800

    index

---
+++ index.ts
@@ -0,0 +1,9 @@
+export * as array from './lib/array'
+
+export * as common from './lib/common'
+
+export * as number from './lib/number'
+
+export * as object from './lib/object'
+
+export * as string from './lib/string' 
