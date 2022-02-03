d2966d924cb467bc56ee4a3c11d8cba4efbedcfb
kawayiLinLin <1936635053@qq.com>
2022-02-03 13:28:39 +0800

    add: ci

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,9 @@
+const package = require('../../package.json')
+
 module.exports = {
   title: "TypeScript-Lodash",
   description: "TypeScript 的工具类型集合",
+  base: `/${package.name}/`,
   evergreen: true,
   themeConfig: {
     lastUpdated: '最后更新时间',
