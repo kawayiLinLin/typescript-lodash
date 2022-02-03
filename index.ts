1f820b0704855206b651cd5e94260a3c88564bb8
kawayiLinLin <1936635053@qq.com>
2022-02-03 14:42:34 +0800

    update: index

--- index.ts
+++ index.ts
@@ -9,5 +9,3 @@ export * as object from './lib/object'
 export * as string from './lib/string'
 
 export * as function from './lib/function'
-
-// 嵌套实例过深的问题 https://github.com/microsoft/TypeScript/issues/37613
